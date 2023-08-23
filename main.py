96f33e223e849357845e94fd36c4d42f852c0233
Pratham Doshi <37569372+Prathamdoshi@users.noreply.github.com>
2023-08-23 15:59:23 -0700

    Remove white background
    successfully was able to remove the white background out of all the clothing pictures.

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
 # importing libraries
 from rembg import remove
 from PIL import Image
+import os
+
+
+"""
+Function: remove_background
+Params: Input directory path & Output directory path
+Returns: None
+Purpose: Removes background of all the files in the input directory and pastes in output directory. 
+"""
+
+def remove_background(input_path, output_path):
+
+    for filename in os.listdir(input_path):
+
+        input_path_formatted = os.path.join(input_path, filename)
+        output_path_formatted = os.path.join(output_path, filename)
+        output_path_formatted = output_path_formatted.replace(".jpeg",".png")
+
+        input = Image.open(input_path_formatted)
+        output = remove(input)
+        output.save(output_path_formatted)
+
+input_path = "/Users/prathamdoshi/PycharmProjects/ReColor/raw_images/"
+output_path = "/Users/prathamdoshi/PycharmProjects/ReColor/images/"
+
+# remove white background out of all the files in raw_images and images
+remove_background(input_path,output_path)
 
